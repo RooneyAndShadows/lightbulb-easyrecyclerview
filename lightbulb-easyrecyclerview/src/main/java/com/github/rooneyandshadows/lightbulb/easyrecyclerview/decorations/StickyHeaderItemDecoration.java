ebfd870d5a9b1f0b7cdd988d80532ea51c9108ab
rooney123777 <rooney123777@gmail.com>
2022-04-26 16:35:53 +0300

    version 1.0.5 release

--- lightbulb-easyrecyclerview/src/main/java/com/github/rooneyandshadows/lightbulb/easyrecyclerview/decorations/StickyHeaderItemDecoration.java
+++ lightbulb-easyrecyclerview/src/main/java/com/github/rooneyandshadows/lightbulb/easyrecyclerview/decorations/StickyHeaderItemDecoration.java
@@ -5,42 +5,43 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import org.jetbrains.annotations.NotNull;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class StickyHeaderItemDecoration extends RecyclerView.ItemDecoration {
-    private StickyHeaderInterface mListener;
-    private int mStickyHeaderHeight;
 
-    public StickyHeaderItemDecoration(@NonNull StickyHeaderInterface listener) {
-        mListener = listener;
+    private StickyHeaderInterface mListener;
+    private Integer mHeaderHeight;
+
+    public StickyHeaderItemDecoration(StickyHeaderInterface listener) {
+        this.mListener = listener;
     }
 
     @Override
-    public void onDrawOver(@NotNull Canvas c, @NotNull RecyclerView parent, @NotNull RecyclerView.State state) {
+    public void onDrawOver(@NonNull Canvas c, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
         super.onDrawOver(c, parent, state);
         View topChild = parent.getChildAt(0);
         if (topChild == null) {
             return;
         }
+
         int topChildPosition = parent.getChildAdapterPosition(topChild);
         if (topChildPosition == RecyclerView.NO_POSITION) {
             return;
         }
+
         int headerPos = mListener.getHeaderPositionForItem(topChildPosition);
-        if (headerPos == -1)
-            return;
         View currentHeader = getHeaderViewForItem(headerPos, parent);
         fixLayoutSize(parent, currentHeader);
-        int headerBottom = currentHeader.getBottom();
-        View childInContact = getChildInContact(parent, headerBottom, headerPos);
+        int contactPoint = currentHeader.getBottom();
+        View childInContact = getChildInContact(parent, contactPoint, headerPos);
 
         if (childInContact != null && mListener.isHeader(parent.getChildAdapterPosition(childInContact))) {
             moveHeader(c, currentHeader, childInContact);
             return;
         }
+
         drawHeader(c, currentHeader);
     }
 
@@ -65,26 +66,35 @@ public class StickyHeaderItemDecoration extends RecyclerView.ItemDecoration {
         c.restore();
     }
 
-    private View getChildInContact(RecyclerView parent, int headerBottom, int currentHeaderPos) {
+    private View getChildInContact(RecyclerView parent, int contactPoint, int currentHeaderPos) {
         View childInContact = null;
-        int nextHeaderViewPos = -1;
         for (int i = 0; i < parent.getChildCount(); i++) {
-            View childView = parent.getChildAt(i);
-            int posInAdapter = parent.getChildAdapterPosition(childView);
-            if (currentHeaderPos < posInAdapter && posInAdapter == mListener.getHeaderPositionForItem(posInAdapter)) {
-                nextHeaderViewPos = i;
-                break;
+            int heightTolerance = 0;
+            View child = parent.getChildAt(i);
+
+            //measure height tolerance with child if child is another header
+            if (currentHeaderPos != i) {
+                boolean isChildHeader = mListener.isHeader(parent.getChildAdapterPosition(child));
+                if (isChildHeader) {
+                    heightTolerance = mHeaderHeight - child.getHeight();
+                }
             }
-        }
-        if (nextHeaderViewPos != -1) {
-            View firstChild = parent.getChildAt(0);
-            View nextHeaderView = parent.getChildAt(nextHeaderViewPos);
-            int nextHeaderTop = nextHeaderView.getTop();
-            int firstChildBottom = firstChild.getBottom();
-            if (nextHeaderTop <= headerBottom)
-                return nextHeaderView;
-            else if (firstChildBottom > headerBottom)
-                return firstChild;
+
+            //add heightTolerance if child top be in display area
+            int childBottomPosition;
+            if (child.getTop() > 0) {
+                childBottomPosition = child.getBottom() + heightTolerance;
+            } else {
+                childBottomPosition = child.getBottom();
+            }
+
+            if (childBottomPosition > contactPoint) {
+                if (child.getTop() <= contactPoint) {
+                    // This child overlaps the contactPoint
+                    childInContact = child;
+                    break;
+                }
+            }
         }
         return childInContact;
     }
@@ -106,7 +116,8 @@ public class StickyHeaderItemDecoration extends RecyclerView.ItemDecoration {
         int childHeightSpec = ViewGroup.getChildMeasureSpec(heightSpec, parent.getPaddingTop() + parent.getPaddingBottom(), view.getLayoutParams().height);
 
         view.measure(childWidthSpec, childHeightSpec);
-        view.layout(0, 0, view.getMeasuredWidth(), mStickyHeaderHeight = view.getMeasuredHeight());
+
+        view.layout(0, 0, view.getMeasuredWidth(), mHeaderHeight = view.getMeasuredHeight());
     }
 
     public interface StickyHeaderInterface {
