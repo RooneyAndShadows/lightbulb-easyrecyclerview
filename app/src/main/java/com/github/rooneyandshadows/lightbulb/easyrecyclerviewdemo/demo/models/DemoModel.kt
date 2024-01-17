2fc63a8a7828e16eb22185a07b18442b8fe08f31
rooney123777 <rooney123777@gmail.com>
2024-01-17 21:05:02 +0200

    Demo work.

--- app/src/main/java/com/github/rooneyandshadows/lightbulb/easyrecyclerviewdemo/demo/models/DemoModel.kt
+++ app/src/main/java/com/github/rooneyandshadows/lightbulb/easyrecyclerviewdemo/demo/models/DemoModel.kt
@@ -1,16 +1,25 @@
 package com.github.rooneyandshadows.lightbulb.easyrecyclerviewdemo.demo.models
 
 import androidx.databinding.Bindable
-import com.github.rooneyandshadows.lightbulb.commons.databinding.ObservableProperty
+import com.github.rooneyandshadows.lightbulb.apt.annotations.LightbulbParcelable
 import com.github.rooneyandshadows.lightbulb.easyrecyclerviewdemo.BR
 import com.github.rooneyandshadows.lightbulb.recycleradapters.abstraction.data.EasyAdapterObservableDataModel
 
+@LightbulbParcelable
 class DemoModel(title: String, subtitle: String) : EasyAdapterObservableDataModel() {
     @get:Bindable
-    var title: String by ObservableProperty(title, BR.title)
+    var title: String = title
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.title)
+        }
 
     @get:Bindable
-    var subtitle: String by ObservableProperty(subtitle, BR.subtitle)
+    var subtitle: String = subtitle
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.subtitle)
+        }
 
     @get:Bindable("title")
     override val itemName: String
