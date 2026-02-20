eae1c38609b98faa5327f4183014bbf72e866721
rooney123777 <rooney123777@gmail.com>
2026-02-21 00:27:54 +0200

    version 2.4.4 - VerticalAndHorizontalSpaceItemDecoration bug when using horizontal layout manager fixed

--- lightbulb-easyrecyclerview/src/main/java/com/github/rooneyandshadows/lightbulb/easyrecyclerview/item_decorations/VerticalAndHorizontalSpaceItemDecoration.kt
+++ lightbulb-easyrecyclerview/src/main/java/com/github/rooneyandshadows/lightbulb/easyrecyclerview/item_decorations/VerticalAndHorizontalSpaceItemDecoration.kt
@@ -7,17 +7,33 @@ import com.github.rooneyandshadows.lightbulb.easyrecyclerview.item_decorations.b
 
 class VerticalAndHorizontalSpaceItemDecoration(
     private val verticalSpacing: Int,
-    private val horizontalSpacing: Int,
+    private val horizontalSpacing: Int
 ) : EasyRecyclerItemDecoration() {
 
     constructor(spacing: Int) : this(spacing, spacing)
 
-    @Override
-    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
-        outRect.left = horizontalSpacing
-        outRect.right = horizontalSpacing
-        outRect.top = verticalSpacing
-        if (parent.getChildAdapterPosition(view) == parent.adapter!!.itemCount - 1)
+    override fun getItemOffsets(
+        outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State
+    ) {
+        val position = parent.getChildAdapterPosition(view)
+        val itemCount = parent.adapter?.itemCount ?: 0
+
+        val layoutManager = parent.layoutManager
+        val isVertical = layoutManager?.canScrollVertically() == true
+        val isHorizontal = layoutManager?.canScrollHorizontally() == true
+
+        if (isVertical) {
+            outRect.top = if (position == 0) verticalSpacing else verticalSpacing / 2
+            outRect.bottom = if (position == itemCount - 1) verticalSpacing else verticalSpacing / 2
+            outRect.left = horizontalSpacing
+            outRect.right = horizontalSpacing
+        } else if (isHorizontal) {
+            outRect.left = if (position == 0) horizontalSpacing else horizontalSpacing / 2
+            outRect.right = if (position == itemCount - 1) horizontalSpacing else horizontalSpacing / 2
+            outRect.top = verticalSpacing
             outRect.bottom = verticalSpacing
+        } else {
+            outRect.set(horizontalSpacing, verticalSpacing, horizontalSpacing, verticalSpacing)
+        }
     }
 }
